d8c281320adf668f2dca5d2c41b3dd307e710cea
betoxxD <betoxxdiosnava@gmail.com>
2020-11-05 17:32:56 -0600

    Cambio para que los nombres coincidan

--- app/src/main/java/com/example/notes/ui/notes/NotesFragment.java
+++ app/src/main/java/com/example/notes/ui/notes/NotesFragment.java
@@ -1,4 +1,4 @@
-package com.example.notes.ui.slideshow;
+package com.example.notes.ui.notes;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.notes.R;
 
-public class SlideshowFragment extends Fragment {
+public class NotesFragment extends Fragment {
 
-    private SlideshowViewModel slideshowViewModel;
+    private NotesViewModel notesViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        slideshowViewModel =
-                ViewModelProviders.of(this).get(SlideshowViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        notesViewModel =
+                ViewModelProviders.of(this).get(NotesViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        final TextView textView = root.findViewById(R.id.text_home);
+        notesViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
--- app/src/main/java/com/example/notes/ui/reminders/RemindersViewModel.java
+++ app/src/main/java/com/example/notes/ui/reminders/RemindersViewModel.java
@@ -1,14 +1,14 @@
-package com.example.notes.ui.gallery;
+package com.example.notes.ui.reminders;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class GalleryViewModel extends ViewModel {
+public class RemindersViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public GalleryViewModel() {
+    public RemindersViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is gallery fragment");
     }
